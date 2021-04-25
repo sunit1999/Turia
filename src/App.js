77c3cc8ddb8ef06085d5e137477237a492cce632
sunit1999 <sunitsharma1999@gmail.com>
2021-04-25 23:26:30 +0530

    updated task

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
-// import CircularSlider from '@fseehawer/react-circular-slider';
 
 import './App.css';
 import Clock from './components/clock';
 import TaskBox from './components/task';
 
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      timer: 0
-    }
-  }
-
 
   render() { 
     return (
